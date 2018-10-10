fdd20cddf63241d59978a2166c1e3ec9ee9cfb34
Guilherme Esteves <guilhermesteves@icloud.com>
2018-10-10 16:52:09 -0300

    add root

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+function InitialResize() {
+  const lengthr = document.documentElement.clientWidth;
+  const heightr = document.documentElement.clientHeight;
+
+  const source = document.getElementById('background');
+  source.style.height = heightr+'px'; // uses current height of page
+  source.style.width = lengthr+'px'; // Uses current width of page
+}
+
+function addEvent(element, type, listener) {
+  if(element.addEventListener) {
+    element.addEventListener(type, listener, false);
+  }else if(element.attachEvent) {
+    element.attachEvent("on"+type, listener);
+  }
+}
+
+addEvent(window, "load", InitialResize);
+addEvent(window, "resize", InitialResize);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
