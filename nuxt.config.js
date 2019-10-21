5d1cdc52a65b9dcbe937fcefb05ebaef1633c784
Guilherme Esteves <guilhermesteves@icloud.com>
2019-10-21 11:29:24 -0300

    add head tags in nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,12 +10,68 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'language', content: 'pt-br' },
+      { name: 'robots', content: 'index, follow' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
       {
-        hid: 'description',
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+      },
+      { name: 'theme-color:', content: '#000000' },
+      { name: 'apple-mobile-web-app-capable:', content: 'yes' },
+      {
+        name: 'apple-mobile-web-app-status-bar-style:',
+        content: 'black-translucent:'
+      },
+
+      {
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content:
+          'Quer saber que cor hexadecimal é agora? Seus problemas acabaram!'
+      },
+      { name: 'keywords', content: 'que cor é agora, cor, horas' },
+      { name: 'distribution', content: 'Global' },
+      { name: 'rating', content: 'General' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'revisit-after', content: '30 days' },
+      { name: 'creator', content: 'Guilherme Esteves' },
+      { name: 'publisher', content: 'Guilherme Esteves' },
+
+      // Open Graph
+      {
+        property: 'og:url',
+        content: 'https://quecoreagora.guilhermeesteves.com'
+      },
+      { property: 'og:title', content: 'Que cor é agora?' },
+      { property: 'og:site_name', content: 'Que cor é agora?' },
+      {
+        property: 'og:description',
+        content:
+          'Quer saber que cor hexadecimal é agora? Seus problemas acabaram!'
+      },
+      { property: 'og:image', content: '/img/redes-sociais.png' },
+      { property: 'og:image:type', content: 'image/jpeg' },
+      { property: 'og:image:width', content: '563' },
+      { property: 'og:image:height', content: '563' },
+      { property: 'og:type', content: 'website' },
+      { property: 'fb:admins', content: '100001899591977' },
+
+      // Twitter
+      { name: 'twitter:card', content: 'summary' },
+      {
+        name: 'twitter:site',
+        content: 'https://quecoreagora.guilhermeesteves.com'
+      },
+      { name: 'twitter:title', content: 'Que cor é agora?' },
+      {
+        name: 'twitter:description',
+        content:
+          'Quer saber que cor hexadecimal é agora? Seus problemas acabaram!'
+      },
+      { name: 'twitter:creator', content: '@guilhermesteves' },
+      { name: 'twitter:image', content: '/img/redes-sociais.png' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
